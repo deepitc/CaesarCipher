48cffba9d2deda1e50a62b32565984179d76f0f7
Deepit Chandgothia <chandgot2@grinnell.edu>
2022-09-30 11:37:18 -0500

    CaesarCipher Code

---
+++ CaesarCipher.java
@@ -0,0 +1,62 @@
+// Name of author(s) : DEEPIT CHANDGOTHIA
+// Assignment Name: CAESAR CIPHER ASSIGNMENT 1
+// DUE : 02/09/2022
+// Written/online sources used: Looked at some examples of caesar cipher off the internet
+// Help obtained: Friend helped with testing
+// I confirm that the above list of sources is complete AND that
+// I have not talked to anyone else (e.g., CSC 207 students) about the solution to this problem.
+
+// CAESAR CIPHER 
+
+public class CaesarCipher {
+
+    public static int charToCode(char letter) {
+        int lower_base = (int) 'a';
+        int ascii_value = (int) letter;   // get the ascii value of the character
+        return ascii_value - lower_base;  // get the offset from 'a'
+    }
+
+    public static char codeToChar(int value) {
+        int base = (int) 'a';
+        return (char) (value + base);
+    }
+ 
+    public static int  modulo(int shifted_letter) {
+        int normalized;
+        if (shifted_letter < 0) {
+            // Negative
+            normalized = 26 + shifted_letter;
+        } else if (shifted_letter > 25) {
+            // More than 25
+            normalized = shifted_letter % 26;
+        } else {
+            normalized = shifted_letter;
+        }
+
+        return normalized;
+        
+    }
+
+    public static char encrypt(char letter, int num) {
+        // change the character to a value
+        int value = charToCode(letter);
+
+        // calculate the shifted value using n
+        int shifted_value = value + num;
+
+        // normalize the shifted value
+        int new_value = modulo(shifted_value);
+
+        // return the value to a character, which is returned
+        return codeToChar(new_value);
+        
+    }
+
+    public static char decrypt(char ch, int n) {
+        int value = charToCode(ch);
+        int shifted = value - n;
+        int newvalue = modulo(shifted);
+
+        return codeToChar(newvalue);
+    }
+}
---
+++ CaesarCipherTester.java
@@ -0,0 +1,76 @@
+// Name of author(s) : DEEPIT CHANDGOTHIA
+// Assignment Name: CAESAR CIPHER ASSIGNMENT 1
+// DUE : 02/09/2022
+// Written/online sources used: Looked at some examples of caesar cipher off the internet
+// Help obtained: Friend helped with testing
+// I confirm that the above list of sources is complete AND that
+// I have not talked to anyone else (e.g., CSC 207 students) about the solution to this problem.
+
+// CAESAR CIPHER TESTER
+
+import java.util.Scanner;
+
+public class CaesarCipherTester {
+	public static void main(String [] args) {
+            // Creates a new scanner object which reads user
+            Scanner grinnell = new Scanner(System.in);
+
+            // Prints the purpose of this program
+            System.out.println("This program encrypts and decrypts messages using the Caesar Cipher.");
+
+            System.out.println("Would you like to encode or decode a message?");
+            String choice = grinnell.nextLine(); // Takes user input for choice
+
+            // A friend while testing my program wrote Encode
+            // so to include that I converted input to lower case
+            String choice_lower_case = choice.toLowerCase();
+
+            if (choice_lower_case.equals("encode")) {
+                System.out.println("Enter the string to encode: ");
+                String str = grinnell.nextLine();
+                encodeTable(str);
+            }
+            else if (choice_lower_case.equals("decode")) {
+                System.out.println("Enter the string to decode: ");
+                String str = grinnell.nextLine();
+                decodeTable(str);
+            }
+            else
+                {
+                    System.out.println("Valid options are \"encode\" or \"decode\"");
+                    return;
+                }
+            System.out.println();
+            
+	}
+
+    public static void encodeTable (String normal_text) {
+        int text_length = normal_text.length();
+        char next;
+        for (int i = 0; i < 26; i++) {
+            System.out.print("n = " + i + ": ");
+            for (int j = 0; j < text_length ; j++) { 
+                next = normal_text.charAt(j);
+                System.out.print(CaesarCipher.encrypt(next, i));
+            }
+            System.out.println();
+        }
+
+    }
+
+    public static void decodeTable (String encrypted_text) {
+        int text_length = encrypted_text.length();
+        char next;
+        for (int i = 0; i < 26; i++) { 
+            System.out.print("n = " + i + ": ");
+            for (int j = 0; j < text_length; j++) {
+                next = encrypted_text.charAt(j);
+                System.out.print(CaesarCipher.decrypt(next, i));
+            }
+            System.out.println();
+
+        }
+
+    }
+    
+}
